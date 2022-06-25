ba5ebc4cf9fedf1f94434bfe4eb4c5be379f2720
mkhawaraslam <mkhawaraslam0@gmail.com>
2022-06-25 20:08:25 +0500

    contact added + contact list set/get from localstorage + displayed

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,18 +1,44 @@
 import React from "react";
 
 class AddContact extends React.Component {
+  state = {
+    name: '',
+    email: ''
+  }
+
+  add = (e) => {
+    e.preventDefault();
+    if ( this.state.name === '' || this.state.email === '') {
+      alert('All fileds are manadetory!')
+      return
+    }
+    this.props.contactHandler(this.state);
+    this.setState({name: '', email: ''});
+  }
   render() {
     return (
       <div className="add-contact-block ui main">
         <h2>Add Contact</h2>
-        <form className="ui form">
+        <form className="ui form"  onSubmit={this.add}>
           <div className="field">
             <label>Name</label>
-            <input type="text" placeholder="Name" name="name" />
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={this.state.name}
+              onChange={(e) => {this.setState({name: e.target.value})}}
+            />
           </div>
           <div className="field">
             <label>Email</label>
-            <input type="text" placeholder="Email" name="email" />
+            <input
+              type="text"
+              placeholder="Email"
+              name="email"
+              value={this.state.email}
+              onChange={(e) => {this.setState({email: e.target.value})}}
+            />
           </div>
           <button className="ui button blue">Add</button>
         </form>
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 import AddContactList from './ContactList';
 
 function App() {
+  const LOCAL_STORAGE_KEY = 'contacts';
+  const retrievedContacts = localStorage.getItem(LOCAL_STORAGE_KEY);
+  const contactsList = retrievedContacts ? JSON.parse(retrievedContacts) : [];
+  const [contacts, setContacts] = useState(contactsList);
+
+  const contactHandler = (contact) => {
+    setContacts([...contacts, contact])
+  }
+
+  // TODO: here data is not fecthing on page reload.
+  // useEffect(() => {
+  //   const retrievedContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+  //   if (retrievedContacts) setContacts(retrievedContacts);
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <div>
+    <div className='ui container'>
       <Header />
-      <AddContact />
-      <AddContactList />
+      <AddContact contactHandler={contactHandler} />
+      <AddContactList contacts={contacts} />
     </div>
   );
 }
